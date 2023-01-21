0fc8050ab65ca8acb4368266b3a9817e3e90c5fb
Evan Wright <ecwright595@my.pittcc.edu>
2023-01-21 18:53:20 -0500

    revamp default functions template

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,9 +1,9 @@
+const express = require('express');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+admin.initializeApp();
+const app = express();
+
+app.get('/', (req, res) => res.status(200).send('Hey there!'));
+exports.app = functions.https.onRequest(app);
